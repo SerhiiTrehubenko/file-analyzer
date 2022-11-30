f00f7efaf50d98c6ad6cacebbc307fc2f32cc59b
Serhii Trehubenko <tsa85.ca@gmail.com>
2022-11-30 10:56:42 +0200

    FileAnalyzer - tests expanded and refactoring

--- src/main/java/com/tsa/analyzer/FileAnalyzer.java
+++ src/main/java/com/tsa/analyzer/FileAnalyzer.java
@@ -2,27 +2,21 @@ package com.tsa.analyzer;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class FileAnalyzer {
 
     private static List<String> coincides;
 
     public static void main(String[] args) {
-        if (args.length < 2)
-            throw new IllegalArgumentException("You should provide only TWO arguments:\n" +
-                    " the first is a PATH to a file, the second is a WORD to find");
-        String wordToFind = args[1];
-        Path path = Path.of(args[0]);
-        File file = path.toFile();
-        if (!file.canExecute()) throw new RuntimeException("there is a problem with the Path");
+
+        Object[] parameters = checkParameters(args);
+        String wordToFind = (String) parameters[1];
+        File file = (File) parameters[0];
 
         coincides = new ArrayList<>();
-        List<Character> characterList = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
 
         try (var inputFile = new FileInputStream(file)) {
             int readByte;
@@ -30,26 +24,47 @@ public class FileAnalyzer {
             while ((readByte = inputFile.read()) != -1) {
                 char castedChar = (char) readByte;
 
-                if (isCrlf(castedChar)) {
-                    characterList.add(castedChar);
+                if (!isCrlf(castedChar)) {
+                    stringBuilder.append(castedChar);
                 }
 
                 if (isSentenceSeparator(castedChar)) {
-                    StringBuilder sentence = new StringBuilder();
-                    characterList.forEach(sentence::append);
-
-                    String sentenceToString = sentence.toString().trim();
-
-                    if (sentenceToString.contains(wordToFind)) {
-                        coincides.add(sentenceToString);
-                    }
-                    characterList.clear();
+                    getSentenceContainsWord(coincides, stringBuilder, wordToFind);
                 }
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
+        printResult(coincides);
+    }
+
+    static void getSentenceContainsWord(List<String> coincides,
+                                        StringBuilder stringBuilder,
+                                        String wordToFind) {
+
+        String sentenceToString = stringBuilder.toString().trim();
+
+        if (sentenceToString.contains(wordToFind)) {
+            coincides.add(sentenceToString);
+        }
+        stringBuilder.delete(0, stringBuilder.length());
+    }
+
+    static List<String> getCoincides() {
+        return coincides;
+    }
+
+    static boolean isCrlf(char castedChar) {
+        return castedChar == 10 | castedChar == 13;
+    }
+
+    static boolean isSentenceSeparator(char castedChar) {
+        return String.valueOf(castedChar).equals(".") |
+                String.valueOf(castedChar).equals("!") |
+                String.valueOf(castedChar).equals("?");
+    }
+
+    static void printResult(List<String> coincides) {
         System.out.println("Number of coincides: " + coincides.size());
         System.out.println();
         int count = 1;
@@ -59,16 +74,18 @@ public class FileAnalyzer {
         }
     }
 
-    static List<String> getCoincides() {
-        return coincides;
-    }
-    private static boolean isCrlf(char castedChar) {
-        return castedChar != 10 & castedChar != 13;
-    }
-
-    private static boolean isSentenceSeparator(char castedChar) {
-        return String.valueOf(castedChar).equals(".") |
-                String.valueOf(castedChar).equals("!") |
-                String.valueOf(castedChar).equals("?");
+    static Object[] checkParameters(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("You should provide only TWO arguments:\n" +
+                    " the first is a PATH to a file, the second is a WORD to find");
+        }
+        Object[] parameters = new Object[2];
+        parameters[1] = args[1];
+        File file = new File(args[0]);
+        parameters[0] = file;
+        if (!file.canExecute()) {
+            throw new RuntimeException("there is a problem with the Path: " + file.getAbsolutePath());
+        }
+        return parameters;
     }
 }
--- src/test/java/com/tsa/analyzer/FileAnalyzerTest.java
+++ src/test/java/com/tsa/analyzer/FileAnalyzerTest.java
@@ -1,15 +1,141 @@
 package com.tsa.analyzer;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static com.tsa.analyzer.FileAnalyzer.*;
 
 public class FileAnalyzerTest {
 
+    @DisplayName("Test checkParameters(), IllegalArgumentException is thrown when" +
+            " a quantity of parameters is less than two")
     @Test
-    void testClass() {
+    void testCheckParametersQuantityLessThenTwo() {
+        String[] parameters = {""};
+
+        assertThrows(IllegalArgumentException.class, () -> checkParameters(parameters));
+    }
+
+    @DisplayName("Test checkParameters(), IllegalArgumentException is thrown when" +
+            " a quantity of parameters is bigger than two")
+    @Test
+    void testCheckParametersQuantityBiggerThenTwo() {
+        String[] parameters = {"", "", ""};
+
+        assertThrows(IllegalArgumentException.class, () -> checkParameters(parameters));
+    }
+
+    @DisplayName("Test checkParameters(), RuntimeException is thrown when" +
+            " a path to file is not valid")
+    @Test
+    void testCheckParametersFilePathIsNotValid() {
+        String[] parameters = {"", ""};
+
+        assertThrows(RuntimeException.class, () -> checkParameters(parameters));
+    }
+
+    @DisplayName("Test checkParameters(), with eligible parameters")
+    @Test
+    void testCheckParametersWithEligibleParameters() {
+        String path = "F:\\0_CODING\\11_Anatol_java\\file-analyzer\\text.txt";
+        String word = "language";
+        String[] parameters = {path, word};
+
+        Object[] result = checkParameters(parameters);
+
+        assertEquals(path, ((File) result[0]).getAbsolutePath());
+        assertEquals(word, result[1]);
+    }
+
+    @DisplayName("Test isSentenceSeparator(), returns TRUE when char is '.', '!', '?'")
+    @Test
+    void testIsSentenceSeparatorAccordingRequirements() {
+        char[] chars = {'.', '!', '?'};
+
+        assertTrue(isSentenceSeparator(chars[0]));
+        assertTrue(isSentenceSeparator(chars[1]));
+        assertTrue(isSentenceSeparator(chars[2]));
+    }
+
+    @DisplayName("Test isSentenceSeparator(), returns FALSE when char is NOT '.', '!', '?'")
+    @Test
+    void testIsSentenceSeparatorRequirementsAreNotValid() {
+        char[] chars = {'d', ' ', '\n'};
+
+        assertFalse(isSentenceSeparator(chars[0]));
+        assertFalse(isSentenceSeparator(chars[1]));
+        assertFalse(isSentenceSeparator(chars[2]));
+    }
+
+    @DisplayName("Test isCrlf(), returns TRUE when char is '\n', '\r'")
+    @Test
+    void testisCrlfReturnsTrueWhenCRLF() {
+        char[] chars = {'\r', '\n'};
+
+        assertTrue(isCrlf(chars[0]));
+        assertTrue(isCrlf(chars[1]));
+    }
+
+    @DisplayName("Test isCrlf(), returns FALSE when char is not '\n', '\r'")
+    @Test
+    void testisCrlfReturnsFalseWhenNotCRLF() {
+        char[] chars = {'a', ' '};
+
+        assertFalse(isCrlf(chars[0]));
+        assertFalse(isCrlf(chars[1]));
+    }
+
+    @DisplayName("Test getSentenceContainsWord(), <Word is present> fill List with a sentence when the sentence contains needed word")
+    @Test
+    void testGetSentenceContainsWordWordIsPresent() {
+        String word = "language";
+        String text = "This is true of both spoken/written languages and programming languages.";
+        List<String> coincides = new ArrayList<>();
+        var stringBuilder = new StringBuilder();
+        stringBuilder.append(text);
+
+        getSentenceContainsWord(coincides, stringBuilder, word);
+
+        assertEquals(1, coincides.size());
+        assertEquals(text, coincides.get(0));
+    }
+
+    @DisplayName("Test getSentenceContainsWord(), <Word is not present> fill List with a sentence when the sentence contains needed word")
+    @Test
+    void testGetSentenceContainsWordWordIsNotPresent() {
+        String word = "language";
+        String text = "Requiring that everything be\n" +
+                "an object (especially all the way down to the lowest level) is a design mistake, but\n" +
+                "banning objects altogether seems equally draconian.";
+
+        List<String> coincides = new ArrayList<>();
+        var stringBuilder = new StringBuilder();
+        stringBuilder.append(text);
+
+        getSentenceContainsWord(coincides, stringBuilder, word);
+
+        assertEquals(0, coincides.size());
+    }
+
+    @Test
+    void testPrintResult() {
+        List<String> list = new ArrayList<>();
+        list.add("Requiring that everything be\n" +
+                "an object (especially all the way down to the lowest level) is a design mistake, but\n" +
+                "banning objects altogether seems equally draconian.");
+
+        printResult(list);
+    }
+
+    @Test
+    void testMain() {
         FileAnalyzer.main(new String[]{"text.txt", "language"});
 
         assertEquals(15, FileAnalyzer.getCoincides().size());
     }
-
 }
